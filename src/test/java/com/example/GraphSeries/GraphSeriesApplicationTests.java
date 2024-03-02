1462be8abf98adcc2c581881f43fd393ef1a7362
Divya Paliwal <div.pali@gmail.com>
2024-03-02 12:32:37 +0530

    first commit

---
+++ src/test/java/com/example/GraphSeries/GraphSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraphSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
